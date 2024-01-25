7d0b7d6d18580178fc3e85bf1b93ac12c3bf3102
Julián Laterza <laterzajulian@gmail.com>
2024-01-25 19:00:17 -0300

    update gamescreen.js

--- RNCourse/gameApp/screens/GameScreen.js
+++ RNCourse/gameApp/screens/GameScreen.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import Title from '../components/Title';
+import PrimaryButton from '../components/PrimaryButton';
+import { useState } from 'react';
 
-function GameScreen () {
+function generateRandomNumber(min, max, exclude) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min; // Con este codigo genero un numero entero.
+
+    if (randomNumber === exclude) {
+        return generateRandomNumber(min, max, exclude);
+    } else {
+        return randomNumber;
+    }
+}
+
+function GameScreen ({userNumber}) {
+    const initialGuess = generateRandomNumber(1, 100, userNumber );
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
     return (
         <View style={styles.screen}>
-            <Title>Game Screen</Title> 
+            <Title>Game Screen</Title>
 
+            <PrimaryButton onPress={generateRandomNumber}>
+                Generar Numero
+            </PrimaryButton>
             <View>
                 <Text>
                     Higher or lower?
