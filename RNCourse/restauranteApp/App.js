b45f897767a10978747bb1937f61dd73ebedf700
Julián Laterza <laterzajulian@gmail.com>
2024-01-28 21:43:48 -0300

    update v1

--- RNCourse/restauranteApp/App.js
+++ RNCourse/restauranteApp/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MealsOverViewScreen from './screens/MealsOverViewScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='auto' />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverViewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
