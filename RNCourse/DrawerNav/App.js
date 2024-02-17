3a38e422a2b0ba89eeef71ac9013b82048da6422
Julián Laterza <laterzajulian@gmail.com>
2024-02-17 00:43:45 -0300

    Create Drawer Navigation

--- RNCourse/DrawerNav/App.js
+++ RNCourse/DrawerNav/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='User' >
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+
+
+    </NavigationContainer>
   );
 }
 
---
+++ RNCourse/DrawerNav/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>Welcome to React Native!</Text>
+        </View>
+    );
+};
+
+export default WelcomeScreen;
