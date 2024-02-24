8ff56077b191e3ecc1b92c453bc9fee187338ea0
Julián Laterza <laterzajulian@gmail.com>
2024-02-24 13:15:16 -0300

    update iconbutton.js

--- RNCourse/restauranteApp/components/IconButton.js
+++ RNCourse/restauranteApp/components/IconButton.js
@@ -1,13 +1,19 @@
 import { Pressable  } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
-
+import { StyleSheet } from "react-native";
 
 function IconButton () {
     return (
-        <Pressable>
-            <Ionicons name="ios-star" size={24} color="black" />
+        <Pressable style={styles.button}>
+            <Ionicons name="star" size={24} color="#ffbe0b" />
         </Pressable>
     );
 }
 
 export default IconButton;
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+    }
+});
